8f1caefba97f9ce9dc9350c0bbf327f1814b2177
John Morris <john@zultron.com>
2014-07-22 15:02:36 -0500

    Add verbose option to cronjob.sh

--- scripts/cronjob.sh
+++ scripts/cronjob.sh
@@ -2,6 +2,16 @@
 
 # Run get-ppa.sh -U and rsync to website
 
+VERBOSE=false
+CMD_VERBOSE=-q
+
+while getopts v ARG; do
+    case $ARG in
+	v) VERBOSE=true; CMD_VERBOSE="" ;;
+    esac
+done
+
+
 # Set directory
 SCRIPT_DIR=$(readlink -f $(dirname $0))
 TOP_DIR=$(readlink -f $SCRIPT_DIR/..)
@@ -11,9 +21,15 @@ TOP_DIR=$(readlink -f $SCRIPT_DIR/..)
 export http_proxy
 
 # Run get-ppa.sh
-$SCRIPT_DIR/get-ppa.sh -q -c all -U
+PPA_CMD="$SCRIPT_DIR/get-ppa.sh ${CMD_VERBOSE} -c all -U"
+! ${VERBOSE} || echo "running '${PPA_CMD}'"
+${PPA_CMD}
 
 # Rsync
 for target in $RSYNC_TARGETS; do
-    eval "rsync -avzq $TOP_DIR/{dists,pool} \$RSYNC_TARGET_$target"
+    TARGETS="$TOP_DIR/dists $TOP_DIR/pool"
+    eval RSYNC_CMD="\"rsync -avz ${CMD_VERBOSE} $TARGETS \
+	\$RSYNC_TARGET_$target\""
+    ! ${VERBOSE} || echo "running '${RSYNC_CMD}'"
+    ${RSYNC_CMD}
 done
